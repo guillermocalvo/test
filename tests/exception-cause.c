795c7a7e857f3ede16d32eafa51c8510991698ba
Guillermo Calvo <guillermo@guillermo.dev>
2025-02-09 00:41:55 +0100

    Delete superfluous test

--- tests/exception-cause.c
+++ tests/exception-cause.c
@@ -1,8 +1,9 @@
 
 # include "testing.h"
 
-static const struct e4c_exception_type RuntimeException = {NULL, "Runtime exception."};
-static const struct e4c_exception_type CustomException = {&RuntimeException, "This is a custom exception"};
+static const struct e4c_exception_type ERROR1 = {NULL, "ERROR 1"};
+static const struct e4c_exception_type ERROR2 = {NULL, "ERROR 2"};
+static const struct e4c_exception_type ERROR3 = {NULL, "ERROR 3"};
 
 /**
  * Finding the cause of an exception
@@ -17,15 +18,45 @@ TEST_CASE{
 
         TRY {
 
-            THROW(CustomException, "This is the original cause of the issue");
+            THROW(ERROR1, "This is the original cause of the issue");
 
-        } CATCH (RuntimeException) {
+        } CATCH (ERROR1) {
 
-            THROW(RuntimeException, "This is the wrapper exception");
+            THROW(ERROR2, "This is the wrapper exception");
         }
 
-    } CATCH (RuntimeException) {
+    } CATCH (ERROR2) {
 
-        TEST_ASSERT_EQUALS(e4c_get_exception()->cause->type, &CustomException);
+        TEST_ASSERT_EQUALS(e4c_get_exception()->cause->type, &ERROR1);
+    }
+
+    TRY {
+
+        TRY {
+
+            TRY {
+
+                THROW(ERROR1, "This is the original cause of the issue");
+
+            } CATCH (ERROR1) {
+
+                THROW(ERROR2, "First wrapper");
+            }
+
+        } CATCH (ERROR2) {
+
+            THROW(ERROR3, "Second wrapper");
+        }
+
+    } CATCH (ERROR3) {
+
+        const struct e4c_exception * exception = e4c_get_exception();
+
+        TEST_ASSERT_EQUALS(exception->cause->type, &ERROR2);
+        TEST_ASSERT_EQUALS(exception->cause->cause->type, &ERROR1);
+
+        fprintf(stderr, "CAUGHT: %s: %s\n", exception->name, exception->message);
+        fprintf(stderr, "  CAUSED BY: %s: %s\n", exception->cause->name, exception->cause->message);
+        fprintf(stderr, "    CAUSED BY: %s: %s\n", exception->cause->cause->name, exception->cause->cause->message);
     }
 }
