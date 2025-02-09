0c83b178d5ea819db4ff2835307e60569a5fb5cf
Guillermo Calvo <guillermo@guillermo.dev>
2025-02-09 02:23:51 +0100

    Refactor "panic" tests

--- tests/panic-block-next.c
+++ tests/panic-block-next.c
@@ -1,14 +1,18 @@
 
+# include <signal.h>
 # include "testing.h"
 
 static struct e4c_context my_context = {0};
 static struct e4c_context * my_supplier(void);
+static void failure(int _);
 
 /**
  * Force panic due to null block.
  *
  */
 TEST_CASE{
+
+    signal(SIGABRT, failure);
 
     e4c_set_context_supplier(my_supplier);
 
@@ -19,4 +23,8 @@ static struct e4c_context * my_supplier(void) {
     free(my_context._innermost_block);
     my_context._innermost_block = NULL;
     return &my_context;
+}
+
+static void failure(int _) {
+    exit(EXIT_FAILURE);
 }
--- tests/panic-context.c
+++ tests/panic-context.c
@@ -1,13 +1,17 @@
 
+# include <signal.h>
 # include "testing.h"
 
 static struct e4c_context * my_supplier(void);
+static void failure(int _);
 
 /**
  * Force panic due to null context.
  *
  */
 TEST_CASE{
+
+    signal(SIGABRT, failure);
 
     e4c_set_context_supplier(my_supplier);
 
@@ -17,4 +21,8 @@ TEST_CASE{
 
 static struct e4c_context * my_supplier(void) {
   return NULL;
+}
+
+static void failure(int _) {
+    exit(EXIT_FAILURE);
 }
